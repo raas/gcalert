6908123691b19d2eded4f37826a5597329f72a00
Andras HORVATH <raas@elvis.elte.hu>
2009-11-09 16:27:31 +0100

    - two threads -- independent wait times for refreshing calendar and   alarming interval - intelligent timezone handling

--- gcalert.py
+++ gcalert.py
@@ -4,12 +4,11 @@
 # This script will periodically check your Google Calendar and display an alarm
 # when that's set for the event. It reads ALL your calendars automatically.
 #
-# Requires: python-notify python-gdata notification-daemon
+# Requires: python-notify python-gdata notification-daemon python-dateutil
 # Also, recommended for SSL: /afs/cern.ch/user/a/ahorvath/public/deb/python-gdata_1.2.4-0ubuntu2ssl_all.deb FIXME
 # Edit below for username/pass
 #
 # FIXME:
-# - time zones (different in calendar than on localhost) !!! Each calendar can have different TZ set! Fcuk
 # - gracious handling of missing pynotify 
 # - command-line options
 # - config file for username/password
@@ -23,22 +22,25 @@ except ImportError:
 
 from gdata.calendar.service import *
 import gdata.service
-import atom.service
 import gdata.calendar
-import atom
 import getopt
 import sys
-import string
 import time
 import urllib
 import pynotify
+import thread
+# magical date parser and timezone handler
+from dateutil.tz import *
+from dateutil.parser import *
 
 # ----------------------------
-sleeptime=30 # seconds
-gcal_time_format='%Y-%m-%dT%H:%M:%S.000+01:00'  # FIXME timezone vs local timezone??!!
+alarm_sleeptime=30 # seconds
+calendar_sleeptime=180 # seconds
+range_days=2 # look this many days in the future
 # ----------------------------
 
 events=[] # all events seen so far that are yet to start
+events_lock=thread.allocate_lock() # hold to access events[]
 alarmed_events=[] # events (occurences etc) already done, minus those in the past
 
 # ----------------------------
@@ -61,6 +63,7 @@ def GetUserCalendars(cs):
 def DateRangeQuery(cs, start_date='2007-01-01', end_date='2007-07-01'):
     el=[] # event occurence list
     for username in GetUserCalendars(cs):
+        # FIXME error checking
         query = gdata.calendar.service.CalendarEventQuery(username, 'private', 'full')
         query.start_min = start_date
         query.start_max = end_date 
@@ -69,9 +72,11 @@ def DateRangeQuery(cs, start_date='2007-01-01', end_date='2007-07-01'):
             for a_when in an_event.when:
                 for a_rem in a_when.reminder:
                     # it's a separate 'event' for each reminder
+                    # start/end times are datetime.datetime() objects here
+                    # created by dateutil.parser.parse()
                     el.append({'title':an_event.title.text, 
-                               'start':a_when.start_time,
-                               'end':a_when.end_time,
+                               'start':parse(a_when.start_time),
+                               'end':parse(a_when.end_time),
                                'minutes':a_rem.minutes})
     return el
 
@@ -79,23 +84,58 @@ def DateRangeQuery(cs, start_date='2007-01-01', end_date='2007-07-01'):
 
 # alarm one event
 def do_alarm(event):
-    # FIXME: time format
-    starttime=time.strftime('%Y-%m-%d %H:%M:%S',time.strptime(event['start'],gcal_time_format))
+    starttime=event['start'].astimezone(tzlocal()).strftime('%Y-%m-%d %H:%M:%S')
     print " ***** ALARM ALARM ALARM %s %s ****  " % ( event['title'],starttime ) 
     # FIXME add an icon here
-    a=pynotify.Notification( event['title'], "Starting: %s" % starttime )
+    a=pynotify.Notification( event['title'], "Starting: %s local time" % starttime )
     # let the alarm stay until it's closed by hand (acknowledged)
     a.set_timeout(0)
     if not a.show():
         print "Failed to send notification!"
 
 
-# ----------------------------
+# -------------------------------------------------------------------------------------------
+# alarming on events is run as a background op
+#
+def process_events_thread():
+    # initialize alarm system
+    if not pynotify.init("Basics"):
+        sys.exit(1)
+    time.sleep(3) # offset :)
+    nowunixtime=time.time()
+    while 1:
+        # throw away old events
+        print "p_e_t: running at %s" % time.ctime()
+        events_lock.acquire()
+        for e in events:
+            e_start_unixtime=int(e['start'].astimezone(tzlocal()).strftime('%s'))
+            if e_start_unixtime<nowunixtime:
+                print "p_e_t: removing %s, is gone" % e
+                events.remove(e)
+                # also free up some memory
+                if e in alarmed_events:
+                    alarmed_events.remove(e)
+            # it starts in the future
+            # check for alarm times if it wasn't alarmed yet
+            elif e not in alarmed_events:
+                # calculate alarm time. If it's now-ish, raise alarm
+                # otherwise, let the event sleep some more
+                alarm_at_unixtime=e_start_unixtime-60*int(e['minutes'])
+                # alarm now if the alarm has 'started'
+                if nowunixtime >= alarm_at_unixtime:
+                    do_alarm(e)
+                    alarmed_events.append(e)
+                else:
+                    print "p_e_t: no alarm yet for %s" % e
+        events_lock.release()
+        print "p_e_t: finished at %s" % time.ctime()
+        # we can't just sleep until the next event as the other thread MIGHT
+        # add something new meanwhile
+        time.sleep(alarm_sleeptime)
 
-# initialize alarm system
-if not pynotify.init("Basics"):
-    sys.exit(1)
-
+# -------------------------------------------------------------------------------------------
+# the main thread will check the calendar every so often
+#
 # login
 cs = CalendarService()
 cs.email = 'probabela98'
@@ -113,41 +153,27 @@ except gdata.service.Error: # seriously, yes
     print 'Check username, password and that the account is enabled.'
     sys.exit(1)
 
-# ----------------------------
+thread.start_new_thread(process_events_thread,())
+
 while 1:
-    print "---- iteration ----"
-    newevents=DateRangeQuery(cs, '2009-11-06','2009-11-07')
-    nowstring=time.strftime(gcal_time_format,time.localtime())
-    nowunixtime=time.time()
+    print "main thread: running at %s " % time.ctime()
+    # today
+    range_start=time.strftime("%Y-%m-%d",time.localtime())
+    # tommorrow, or later
+    range_end=time.strftime("%Y-%m-%d",time.localtime(time.time()+range_days*24*3600))
+    newevents=DateRangeQuery(cs, range_start, range_end)
+    events_lock.acquire()
+    now=time.time()
     # add new events to the list
     for n in newevents:
         if not n in events:
             print 'Received event: %s' % n
             # does it start in the future?
-            if nowstring<n['start']:
+            if now<int(n['start'].astimezone(tzlocal()).strftime('%s')):
                 print "-> future, adding"
                 events.append(n)
             else:
                 print "-> past already"
-    # throw away old events
-    for e in events:
-        if e['start']<nowstring:
-            print "removing %s, is gone" % e
-            events.remove(e)
-            # also free up some memory
-            if e in alarmed_events:
-                alarmed_events.remove(e)
-        # it starts in the future
-        # check for alarm times if it wasn't alarmed yet
-        elif e not in alarmed_events:
-            # calculate alarm time. If it's now-ish, raise alarm
-            # otherwise, let the event sleep some more
-            alarm_at_unixtime=int(time.strftime('%s',(time.strptime(e['start'],gcal_time_format))))-60*int(e['minutes'])
-            # alarm now if the alarm has 'started'
-            if nowunixtime >= alarm_at_unixtime:
-                do_alarm(e)
-                alarmed_events.append(e)
-            else:
-                print "no alarm yet for %s" % e
-    print "...now sleeping %d seconds" % sleeptime
-    time.sleep(sleeptime)
+    events_lock.release()
+    print "main thread: finished at %s, sleeping %d secs " % ( time.ctime(), calendar_sleeptime )
+    time.sleep(calendar_sleeptime)
